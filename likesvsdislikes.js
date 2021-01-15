34a7ce66c6fe2f86ab148d434fd92a4ce6fd61b7
AlidaSchefers <alidadschefers@gmail.com>
2021-01-14 22:20:23 -0500

    initial solution

--- likesvsdislikes.js
+++ likesvsdislikes.js
@@ -1,13 +1,23 @@
 //this algorithm challenge comes from edabit.com 
 //this specific challenge: https://edabit.com/challenge/MNKfYEKghbKjxYbzb
 
+//going from beginning to send.
 function likeOrDislike(arr) {
-	
+    if(arr.length === 0) {return "Nothing"}
+    let finalDisplay = arr[0]
+    for(let i = 1; i < arr.length; i++) {
+        if(arr[i] !== finalDisplay) {
+            // console.log(`arr[i]: ${arr[i]}`)
+            finalDisplay = arr[i]
+        }
+        else{finalDisplay = "Nothing"}
+    }
+    return finalDisplay
 }
 
-// likeOrDislike(["Dislike"]) ➞ "Dislike"
-// likeOrDislike(["Like", "Like"]) ➞ "Nothing"
-// likeOrDislike(["Dislike", "Like"]) ➞ "Like"
-// likeOrDislike(["Like", "Dislike", "Dislike"]) ➞ "Nothing"
+console.log(likeOrDislike(["Dislike"])) //➞ "Dislike"
+console.log(likeOrDislike(["Like", "Like"])) //➞ "Nothing"
+console.log(likeOrDislike(["Dislike", "Like"])) //➞ "Like"
+console.log(likeOrDislike(["Like", "Dislike", "Dislike"])) //➞ "Nothing"
 // If no button is currently active, return "Nothing".
 // If the array is empty, return "Nothing".
