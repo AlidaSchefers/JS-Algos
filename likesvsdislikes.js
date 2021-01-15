ce4ef0bf97cb5a344fa7e4861ec6ba3b8c2e4d20
AlidaSchefers <alidadschefers@gmail.com>
2021-01-14 20:37:43 -0500

    initial commmit

---
+++ likesvsdislikes.js
@@ -0,0 +1,13 @@
+//this algorithm challenge comes from edabit.com 
+//this specific challenge: https://edabit.com/challenge/MNKfYEKghbKjxYbzb
+
+function likeOrDislike(arr) {
+	
+}
+
+// likeOrDislike(["Dislike"]) ➞ "Dislike"
+// likeOrDislike(["Like", "Like"]) ➞ "Nothing"
+// likeOrDislike(["Dislike", "Like"]) ➞ "Like"
+// likeOrDislike(["Like", "Dislike", "Dislike"]) ➞ "Nothing"
+// If no button is currently active, return "Nothing".
+// If the array is empty, return "Nothing".
