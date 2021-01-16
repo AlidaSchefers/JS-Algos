a435218b32d873f82960cd0654d5edc4e1a20ada
AlidaSchefers <alidadschefers@gmail.com>
2021-01-15 22:39:15 -0500

    initial commit

---
+++ upsidedownnumbers.js
@@ -0,0 +1,7 @@
+//this callenge comes from https://www.codewars.com/kata/59f7597716049833200001eb
+
+function solve(x, y) {
+    let upperNum = x > y ? x : y
+    let lowerNum = x > y ? y : x
+    
+   };
