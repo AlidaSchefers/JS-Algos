34584cd6f95b298df1fd2aec6474292a7a548ba8
AlidaSchefers <alidadschefers@gmail.com>
2021-01-15 22:44:16 -0500

    added logic notes

--- upsidedownnumbers.js
+++ upsidedownnumbers.js
@@ -3,5 +3,8 @@
 function solve(x, y) {
     let upperNum = x > y ? x : y
     let lowerNum = x > y ? y : x
-    
+
    };
+//nums that are the same upside-down: 018
+//Careful with 9 and 6. they need to go together with same instances for each. (e.g. 9966 ok and 96 ok, but 996 is not.)
+//9086 is not ok.
